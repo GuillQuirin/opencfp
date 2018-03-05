b13e5f979353e951fa2a3c6e03b876fd4b3d0700
ValentinLENFANT <valentin.lenfant@hotmail.com>
2018-03-05 10:51:57 +0100

    Mesure du temps de l'exécution du script

--- hello.php
+++ hello.php
@@ -1,11 +1,18 @@
 <?php
 
 //Passage de 10 à 12 hello
+$debut = microtime();
 echo "Ce script va afficher 12 fois hello.";
 
 for($i=0;$i<12;$i++)
 {
     echo "Hello";
 }
+$fin = microtime();
+
+$total = $fin - $debut;
+
+echo "Le script a mis " .$total . " secondes à s'éxécuter.";
+echo "Wow ! C'était rapide...";
 
 echo "Et voilà ! Mon super script a fini de s'éxécuter";
